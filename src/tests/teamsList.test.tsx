ef8c72bcb8c78fde949414392619f1ede56fa852
kevinsilva <kevinsilva.j@gmail.com>
2023-12-19 15:47:16 +0000

    [feat]: adds testing for teamsList and configutation to ts config

--- src/tests/teamsList.test.tsx
+++ src/tests/teamsList.test.tsx
@@ -1,12 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import TeamsList from '../components/teamsList';
+import { server } from '../mocks/server';
+import { HttpResponse, http } from 'msw';
 
 describe('TeamsList', () => {
+  it('should render loading text on page when loading data', async () => {
+    render(<TeamsList />);
+
+    const loadingText = screen.getByText('Loading...');
+    expect(loadingText).toBeInTheDocument();
+  });
   it('should render all team names on page', async () => {
     render(<TeamsList />);
 
     const teamItems = await screen.findAllByRole('listitem');
     expect(teamItems.length).toBe(3);
+  });
+  it('renders error message when fetch fails', async () => {
+    server.use(
+      http.get('api/teams', () => {
+        return new HttpResponse(null, {
+          status: 500,
+          statusText: 'Error fetching teams. Please try again.',
+        });
+      })
+    );
+    render(<TeamsList />);
+
+    const errorMessage = await screen.findByText(
+      'Error fetching teams. Please try again.'
+    );
+    expect(errorMessage).toBeInTheDocument();
   });
 });
