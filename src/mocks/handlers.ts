7079dbda95a2d1fd25fdeec996306e861a7dbaaa
kevinsilva <kevinsilva.j@gmail.com>
2023-12-20 19:24:18 +0000

    [feat]: adds testing to teamRoster component

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -13,9 +13,33 @@ export const handlers = [
   http.get('/api/players', () => {
     return HttpResponse.json({
       data: [
-        { id: 1, first_name: 'Player 1', last_name: 'Lastname 1', team_id: 1 },
-        { id: 2, first_name: 'Player 2', last_name: 'Lastname 2', team_id: 2 },
-        { id: 3, first_name: 'Player 3', last_name: 'Lastname 3', team_id: 3 },
+        {
+          id: 1,
+          first_name: 'Firstname 1',
+          last_name: 'Lastname 1',
+          team: {
+            id: 1,
+            full_name: 'Team 1',
+          },
+        },
+        {
+          id: 2,
+          first_name: 'Firstname 2',
+          last_name: 'Lastname 2',
+          team: {
+            id: 2,
+            full_name: 'Team 2',
+          },
+        },
+        {
+          id: 3,
+          first_name: 'Firstname 3',
+          last_name: 'Lastname 3',
+          team: {
+            id: 1,
+            full_name: 'Team 1',
+          },
+        },
       ],
       meta: {
         total_pages: 1,
