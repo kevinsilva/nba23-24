c089adb3bfb8de2448d7fa169742e5c30f17f74c
kevinsilva <kevinsilva.j@gmail.com>
2023-12-28 16:34:31 +0000

    [feat]: adds focus styles to nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,17 +5,25 @@ export default function Navigation() {
     <nav className="font-sans">
       <ul className="flex justify-end gap-4 uppercase text-sm font-semibold text-zinc-500">
         <li className="hover:text-zinc-700">
-          <Link to="/" aria-label="Teams">
+          <Link to="/" aria-label="Teams" className="focus:text-zinc-700">
             Teams
           </Link>
         </li>
         <li className="hover:text-zinc-700">
-          <Link to="/teams/1/roster" aria-label="Roster">
+          <Link
+            to="/teams/1/roster"
+            aria-label="Roster"
+            className="focus:text-zinc-700"
+          >
             Roster
           </Link>
         </li>
         <li className="hover:text-zinc-700">
-          <Link to="/teams/1/games" aria-label="Games">
+          <Link
+            to="/teams/1/games"
+            aria-label="Games"
+            className="focus:text-zinc-700"
+          >
             Games
           </Link>
         </li>
